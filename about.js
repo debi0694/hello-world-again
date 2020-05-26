3a37c5019900d06246c5cf07968b558759d6e622
debi0694 <dypc94@gmail.com>
2020-05-26 18:35:04 +0530

    changes in about.js

--- about.js
+++ about.js
@@ -1,1 +1,19 @@
 console.log('About');
+
+
+const obj = [
+{ 
+    name: 'Debi Prasad',
+    email: 'dypc94@gmail.com',
+    age: 26
+},
+{ 
+    name: 'Debashish',
+    email: '1398deb@gmail.com',
+    age: 21
+}
+]
+
+obj.forEach((ob)=>{
+    console.log(ob[0])
+})
